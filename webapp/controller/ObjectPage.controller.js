b79158bbbf6a9ea8fafc70efd1bd5341586dff95
Ajay S <ajay.s@mindsetconsulting.com>
2023-11-27 16:27:32 +0530

    Filter bar and object page data binding

--- webapp/controller/ObjectPage.controller.js
+++ webapp/controller/ObjectPage.controller.js
@@ -1,20 +1,52 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/Fragment",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-],
-	/**
-	 * @param {typeof sap.ui.core.mvc.Controller} Controller
-	 */
-	function (Controller, JSONModel, Fragment, Filter, FilterOperator) {
-		"use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "meg/workorder/utils/CallUtil",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel, Fragment, Filter, FilterOperator, CallUtil) {
+    "use strict";
 
-		return Controller.extend("meg.workorder.controller.ObjectPage", {
-			onInit: function () {
+    return Controller.extend("meg.workorder.controller.ObjectPage", {
+      onInit: async function () {
+        sap.ui.core.BusyIndicator.show();
+        this.localModel = this.getOwnerComponent().getModel("localModel");
 
-			},
-			
-		});
-	});
+        var oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("RouteObjectPage")
+          .attachPatternMatched(this._onObjectMatched, this);
+      },
+
+      _onObjectMatched: async function (oEvent) {
+        this.WorkOrderId = oEvent.getParameter("arguments").id;
+
+        this.serviceUrl =
+          this.getOwnerComponent().getModel("WorkOrderModel").sServiceUrl;
+        var filter = "?$filter=WorkOrder eq '" + this.WorkOrderId + "'";
+        var expand = "&$expand=OrdOperationNav,EquipBOMItemNav,AddPartItemNav";
+        var data = await CallUtil.callGetData(
+          this.serviceUrl +
+            "/ZUSPPMEG01_WORK_ORDER_HEADERSet" +
+            filter +
+            expand +
+            "&$format=json"
+        );
+
+        data = data.d.results[0];
+        var orderOps = data.OrdOperationNav.results;
+        var equipBOMItems = data.EquipBOMItemNav.results;
+
+        this.localModel.setProperty("/workOrderHeader", data);
+        this.localModel.setProperty("/orderOperations", orderOps);
+        this.localModel.setProperty("/equipBOMItems", equipBOMItems);
+      },
+    });
+  }
+);
--- webapp/utils/FilterUtil.js
+++ webapp/utils/FilterUtil.js
@@ -1,29 +1,41 @@
-sap.ui.define([
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (Filter, FilterOperator) {
-	"use strict";
+sap.ui.define(
+  ["sap/ui/model/Filter", "sap/ui/model/FilterOperator"],
+  function (Filter, FilterOperator) {
+    "use strict";
 
-	return {
+    return {
+      prepareFilters1: function (oFilterValues) {
+        var aFilters = [];
 
-		prepareFilters : function (oFilterValues) {
-			var aFilters = [];
-			
-			Object.keys(oFilterValues).forEach(function(filterName) {
-				if ( Array.isArray(oFilterValues[filterName]) ) {
+        Object.keys(oFilterValues).forEach(function (filterName) {
+          if (Array.isArray(oFilterValues[filterName])) {
+            if (oFilterValues[filterName].length > 0) {
+              aFilters.push(
+                new Filter(
+                  {
+                    filters: oFilterValues[filterName].map(function (value) {
+                      value = value.getProperty("title");
+                      return new Filter(filterName, FilterOperator.EQ, value);
+                    }),
+                  },
+                  false
+                )
+              );
+            }
+          }
+        });
 
-					if ( oFilterValues[filterName].length > 0 ) {
-						aFilters.push( new Filter( { filters: oFilterValues[filterName].map(function(value) {
-							value = value.getProperty("title");
-							return new Filter(filterName, FilterOperator.EQ, value );
-						} ) }, false) );
-					}
-					
-				} 
-		    });
-		    
-		    return aFilters;
-		}
+        return aFilters;
+      },
 
-	};
-});
+      prepareFilters: function (oFilterValues) {
+        var sFilters = "";
+        oFilterValues.forEach(function (filter) {
+          sFilters += filter + " and ";
+        });
+        // sFilters = sFilters.slice(0, -5);
+        return sFilters;
+      },
+    };
+  }
+);
